f28ee971434875eb966690608f85d0d8ac90ed26
Nicolay Rusnachenko <kolyarus@yandex.ru>
2020-10-05 13:25:12 +0300

    Fixed output path (considering installation in a main folder)

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clone --single-branch --branch 0.19.5-bdr-elsevier-2020-py3 git@github.com:nicolay-r/AREkit.git ../core
+git clone --single-branch --branch 0.19.5-bdr-elsevier-2020-py3 git@github.com:nicolay-r/AREkit.git core
